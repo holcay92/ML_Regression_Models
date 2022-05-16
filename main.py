ff2f9148c191d26f8344d90991abbb85c35b357f
H.Halil Olcay <81256755+holcay92@users.noreply.github.com>
2022-05-16 15:32:46 +0300

    simple linear regression

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-dataset = pd.read_csv('Data1.csv')
+dataset = pd.read_csv('piworks.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
@@ -23,6 +23,12 @@ y = le.fit_transform(y)
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 1)
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train[:, 3:] = sc.fit_transform(X_train[:, 3:])
+X_test[:, 3:] = sc.transform(X_test[:, 3:])
+
 print(X)
 print(y)
 print()
