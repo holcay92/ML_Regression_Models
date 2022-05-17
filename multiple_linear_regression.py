17c80d16ca295b5440db7e003468eec441cce6c9
H.Halil Olcay <81256755+holcay92@users.noreply.github.com>
2022-05-17 22:15:12 +0300

    example case added

--- multiple_linear_regression.py
+++ multiple_linear_regression.py
@@ -32,3 +32,7 @@ regressor.fit(X_train, y_train)
 y_pred = regressor.predict(X_test)
 np.set_printoptions(precision=2)
 print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+# Making a single prediction
+# (for example the profit of a startup with R&D Spend = 160000, Administration Spend = 130000,
+# Marketing Spend = 300000 and State = 'California')
+print(regressor.predict([[1, 0, 0, 160000, 130000, 300000]]))
